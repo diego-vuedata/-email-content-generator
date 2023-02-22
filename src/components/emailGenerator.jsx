d80b977d8d3e9566fe64124bdf2abae9b787b391
Diego Soba <diego@vuedata.com>
2023-02-22 00:16:14 -0500

    Fine Tuning model with parameters from form included in response completion

--- src/components/emailGenerator.jsx
+++ src/components/emailGenerator.jsx
@@ -2,13 +2,34 @@
 import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import {Button, Modal} from 'react-bootstrap';
+import Select from 'react-select';
 import axios from "axios";
-import templates from './template.json';
 
 
 function EmailGenerator() {
+    const options = [
+        { value: 'Abandon Cart', label: 'Abandon Cart' },
+        { value: 'Winback Campaign', label: 'Winback Campaign' },
+        { value: 'Notice Pausing', label: 'Notice Pausing' }   
+    ];
+
+    const options_a = [
+        { value: 'Relief Factor 3-Week QuickStart Subscription', label: 'Relief Factor 3-Week QuickStart Subscription' },
+        { value: 'Relief Factor 60-ct Bag Subscription', label: 'Relief Factor 60-ct Bag Subscription' },
+        { value: 'Relief Factor 60-ct Bag Single Order', label: 'Relief Factor 60-ct Bag Single Order' }
+    ];
+
+    const options_b = [
+        { value: 'Energy', label: 'Energy' },
+        { value: 'Sleep', label: 'Sleep' },
+        { value: 'Calm', label: 'Calm' }   
+    ];
+
     const [show, setShow] = useState(false);
-    const [userOption, SetUserOption] = useState('abandoned');
+    const [userOption, SetUserOption] = useState(options[0].value);
+    const [profile, SetProfile] = useState('');
+    const [product, SetProduct] = useState(options_a[0].value);
+    const [detail, SetDetail] = useState(options_b[0].value);
     const [form, setForm] = useState(true);
     const [loading, SetLoading] = useState(false);
     const [response, SetResponse] = useState(false);
@@ -17,25 +38,19 @@ function EmailGenerator() {
     const handleClose = () => {
         setShow(false)
     };
-    const handleChange = (event) => {
-        console.log(event.target.value, 'Diego');
-        SetUserOption(event.target.value);
-    };
+
     const handleSubmit = (event) => {
-        event.preventDefault();       
+        event.preventDefault();         
         setShow(true);
     };
-    const handleChangeTa = (event) => {
-        console.log(event.target.value);
-    }
+
     const handleSubmitModal = async (event) => {
-        event.preventDefault();
-        console.log(userOption);
+        event.preventDefault();         
         SetLoading(true);
-        setForm(false);        
-        try {
-            const response = await axios.post("https://serverchatgptvuedata.herokuapp.com/chat", {
-                req: (userOption == 'abandoned') ? templates.abandoned : (userOption == 'left') ? templates.left : templates.paused,
+        setForm(false);  
+        try {       
+            const response = await axios.post("https://serverchatgptvuedata.herokuapp.com/chat", {                
+                req: `Subject: <${userOption}>\nTo:<${profile}>\nProduct Name:<${detail}>Suscription:<${product}>\nContent:<${userOption}>\n\n###\n\n`
             });
             const res = response.data.message;
             SetContent(res);
@@ -48,15 +63,16 @@ function EmailGenerator() {
 
     return (
         <>
-            <div className="card p-3">{/*col-lg-4 col-md-12*/}
+            <div className="card p-3">
                 <form className="form-signin" onSubmit={handleSubmit}>
                     <h1 className="h3 mb-3 font-weight-normal">Email Content Generator</h1>              
                     <h5 className="h5 mb-3 font-weight-normal text-center">Select your Template</h5>
-                    <select className="form-control" onChange={handleChange}>
-                        <option value="abandoned">Abandon Cart</option>
-                        <option value="left">Winback Campaign</option>
-                        <option value="paused">Notice Pausing</option>
-                    </select>
+                    <Select 
+                        value={options.value}   
+                        defaultValue={options[0]}                     
+                        options={options}                        
+                        onChange={(option) => SetUserOption(option.value)}                        
+                        />
                     <div className="d-grid gap-2 mt-4">
                         <Button variant="outline-secondary" className="btn btn-lg btn-primary btn-block" type="submit">
                             Generate
@@ -74,17 +90,20 @@ function EmailGenerator() {
                                 <input
                                     type="text"
                                     className="w-full border border-gray-400 p-2 rounded-lg"
-                                    placeholder="User Profile Information" />
-                                <select className="form-control mt-2" onChange={handleChange} required>
-                                    <option value="winback">Relief Factor 3-Week QuickStart Subscription</option>
-                                    <option value="pausing">Relief Factor 60-ct Bag Subscription</option>
-                                    <option value="pausing">Relief Factor 60-ct Bag Single Order</option>
-                                </select>
-                                <select className="form-control mt-2" onChange={handleChange} required>
-                                    <option value="winback">Energy</option>
-                                    <option value="pausing">Sleep</option>
-                                    <option value="pausing">Calm</option>
-                                </select>
+                                    placeholder="User Profile Information" 
+                                    onChange={(event)=>SetProfile(event.target.value)}/>
+                                <Select 
+                                    className="text-left mt-2"    
+                                    options={options_a}      
+                                    defaultValue={options_a[0]}
+                                    onChange={(option)=> SetProduct(option.value)}                                     
+                                    />                                    
+                                <Select 
+                                    className="text-left mt-2"  
+                                    options={options_b}
+                                    defaultValue={options_b[0]}                 
+                                    onChange={(option)=> SetDetail(option.value)}
+                                    />                                                                    
                                 <Button variant="outline-secondary" className="btn mt-3 d-inline text-center btn-md btn-primary btn-block" type="submit">
                                     Continue
                                 </Button>
@@ -100,7 +119,7 @@ function EmailGenerator() {
                         {
                             response &&
                             <>
-                                <textarea value={content} className="form-control" onChange={handleChangeTa} cols="30" rows='25'>
+                                <textarea defaultValue={content} className="form-control" cols="30" rows='25'>
                                     
                                 </textarea>
                             </>
